5f35cd2f9d8a6b20b8dbd7f7f4c5d07455b80f33
hygradme <hygradme@gmail.com>
2022-12-30 19:41:43 +0900

    add arduino sketch for ATOM Lite

---
+++ software/arduino_sketch/ModbusRTU_SCSServo.ino
@@ -0,0 +1,72 @@
+#include <SCServo.h>
+
+#include "M5Atom.h"
+
+#include <ModbusRTU.h>
+
+SMS_STS sms_sts;
+unsigned int SERVO_POS = 0;
+#define RXPIN  (32)
+#define TXPIN  (26)
+
+// modbus-rtu setup
+#define SLAVE_ID 1
+#define REGN 10
+#define REG_GRIPPER_POS 128
+#define REG_READ_GRIPPER_POS 257
+
+#define LITE6_OUTPUT0 21
+#define LITE6_OUTPUT1 22
+int lite6_0;
+int lite6_1;
+uint16_t gripper_pos = 1500;
+uint16_t gripper_open_pos = 2200;
+uint16_t gripper_close_pos = 200;
+
+ModbusRTU mb;
+
+void setup() {
+
+  pinMode(LITE6_OUTPUT0, INPUT_PULLUP);
+  pinMode(LITE6_OUTPUT1, INPUT_PULLUP);
+  // put your setup code here, to run once:
+  M5.begin(true, false, true);
+
+  // for servo control uart
+  Serial1.begin(1000000, SERIAL_8N1, 23, 33); 
+  sms_sts.pSerial = &Serial1;
+
+  // modbus rtu
+  Serial2.begin(115200, SERIAL_8N1, RXPIN, TXPIN); 
+  mb.begin(&Serial2);
+  mb.slave(SLAVE_ID);
+
+  mb.addHreg(REG_GRIPPER_POS);
+  mb.Hreg(REG_GRIPPER_POS,  gripper_pos);
+  mb.addIreg(REG_READ_GRIPPER_POS, gripper_pos);
+
+  delay(1000);
+}
+
+void loop() {
+  // with digital input
+  lite6_0 = digitalRead(LITE6_OUTPUT0);
+  lite6_1 = digitalRead(LITE6_OUTPUT1);
+  if(lite6_0 == LOW && lite6_1 == HIGH){
+    sms_sts.WritePosEx(1, gripper_open_pos, 2500, 200);    
+   }
+  if(lite6_0 == HIGH && lite6_1 == LOW){
+    sms_sts.WritePosEx(1, gripper_close_pos, 2500, 200);    
+   }
+
+  // with modbus control mode
+  if(lite6_0 == HIGH && lite6_1 == HIGH){
+
+    gripper_pos = mb.Hreg(REG_GRIPPER_POS);
+    
+    sms_sts.WritePosEx(1, gripper_pos, 2500, 200);  
+  }
+
+  mb.task();
+  yield();
+}
