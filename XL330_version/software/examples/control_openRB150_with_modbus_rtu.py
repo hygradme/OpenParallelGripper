8ea66f8ecf2e663dccc01cfce47eb2fdae85a82c
hygradme <hygradme@gmail.com>
2023-12-02 17:44:24 +0900

    fix example Python script for modbus-rtu in XL330 version

--- XL330_version/software/examples/control_openRB150_with_modbus_rtu.py
+++ XL330_version/software/examples/control_openRB150_with_modbus_rtu.py
@@ -14,8 +14,9 @@ class ParalleGripperOpenRB150:
         self._arm = arm
         self._arm.set_mode(0)
         self._arm.set_state(0)
-        # code = self._arm.set_tgpio_modbus_baudrate(57600)
         code = self._arm.set_tgpio_modbus_baudrate(115200)
+        self._arm.set_tgpio_digital(1, 1)
+        self._arm.set_tgpio_digital(0, 1)
         print('set_tgpio_modbus_baudrate, code={}'.format(code))
         self.close_in_res = 0
         self.open_in_res = 250
@@ -24,12 +25,13 @@ class ParalleGripperOpenRB150:
         data = [0x01, 0x06, 0x00, 0x80]
         data += int_to_2bytes_list(self.open_in_res)
         res = self._arm.getset_tgpio_modbus_data(data)
+        return res
 
     def close(self):
         data = [0x01, 0x06, 0x00, 0x80]
         data += int_to_2bytes_list(self.close_in_res)
-
         res = self._arm.getset_tgpio_modbus_data(data)
+        return res
 
     def move(self, pos):
         assert pos >= self.close_in_res
@@ -37,11 +39,13 @@ class ParalleGripperOpenRB150:
 
         data = [0x01, 0x06, 0x00, 0x80] + int_to_2bytes_list(pos)
         res = self._arm.getset_tgpio_modbus_data(data)
+        return res
 
     def move_out_of_10(self, ratio):
         assert ratio <= 9
         pos = (self.open_in_res - self.close_in_res) * ratio / 10 + self.close_in_res
-        self.move(int(pos))
+        res = self.move(int(pos))
+        return res
 
     def get_pos(self):
         data = [0x01, 0x03, 0x01, 0x01, 0x00, 0x02]
@@ -59,16 +63,20 @@ if __name__ == "__main__":
     time.sleep(1)
 
     for action in action_list:
+        print(action)
+        res = ""
         if action == "o":
-            pga.open()
+            res = pga.open()
 
         elif action == "c":
-            pga.close()
+            res = pga.close()
 
         elif action == "s":
             time.sleep(1)
 
         elif action.isdecimal():
             val = int(action)
-            pga.move_out_of_10(val)
+            res = pga.move_out_of_10(val)
+        print(res)
+
         time.sleep(2)
